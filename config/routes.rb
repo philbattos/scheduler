cf1164f4259c457abc73f3f022153eb42fb14ce1
Raphael Weiner <raphael.weiner@gmail.com>
2013-06-19 22:00:56 -0600

    remove last unused route - woops

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Scheduler::Application.routes.draw do
 
   resources :sessions, only: [:index]
   resources :tracks, only: [:index, :show]
-  resources :conferences, only: [:index, :show]
+  resources :conferences, only: [:index]
   get "/conferences/:slug", to: "conferences#show"
 
 end
